65806fb206c16b74eb2f96dd532115177c4cdacd
Manikanta reddy padala <109054645+Manikanta0997@users.noreply.github.com>
2023-06-23 18:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict_class(image, model):
 
 
 model = load_model()
-st.title('pneumonia detection')
+st.title('Pneumonia Detection')
 
 file = st.file_uploader("Upload an image of a flower", type=["jpg", "png"])
 
